c75aba082025e52341b7040058bd5ae74ea56ace
danielee33 <47678174+danielee3@users.noreply.github.com>
2021-06-29 10:37:23 +0900

    cellType is added to Node

--- lib/src/main/java/research/readjson/Node.java
+++ lib/src/main/java/research/readjson/Node.java
@@ -3,6 +3,7 @@ package research.readjson;
 /** A node within the graph. */
 public class Node {
 	private String pathname;
+	private String cellType;
 	
 	/** 
 	 * Constructor: creates an instance of Node class with the given pathname.
@@ -12,11 +13,24 @@ public class Node {
 		this.pathname = pathname;
 	}
 	
+	public Node(String pathname, String cellType) {
+		this.pathname = pathname;
+		this.cellType = cellType;
+	}
+	
 	/**
 	 * Returns the pathname of the Node 
 	 * @return the pathname of the Node 
 	 * */
 	public String getPathname() {
 		return pathname;
+	}
+	
+	public void setPathname(String pathname) {
+		this.pathname = pathname;
+	}
+	
+	public String getCellType() {
+		return cellType;
 	}
 }
